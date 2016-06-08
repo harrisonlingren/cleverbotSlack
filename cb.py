c8a77ef33a40050c174159e690c6af195ab0ed47
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-08 12:58:04 -0400

    convert from module to class

--- cb.py
+++ cb.py
@@ -1,26 +1,24 @@
 from cleverbot import Cleverbot
 
-forMember = ''
-cb = {}
 
+class cb:
+    forMember = ''
+    cb = {}
 
-def __init__(self):
-    self.forMember = ''
-    self.cb['blank'] = Cleverbot()
+    def __init__(self):
+        self.forMember = ''
+        self.cb['blank'] = Cleverbot()
 
+    def __init__(self, memberid):
+        self.forMember = memberid
+        self.cb[memberid] = Cleverbot()
 
-def __init__(self, memberid):
-    self.forMember = memberid
-    self.cb[memberid] = Cleverbot()
+    def getCb(memberid):
+        if cb[memberid] is not None:
+            return cb[memberid]
+        else:
+            return cb['blank']
 
-
-def getCb(memberid):
-    if cb[memberid] is not None:
-        return cb[memberid]
-    else:
-        return cb['blank']
-
-
-def respond(message, memberid):
-    response = cb[memberid].ask(message)
-    return response
+    def respond(message, memberid):
+        response = cb[memberid].ask(message)
+        return response
