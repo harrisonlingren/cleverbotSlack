b2069e2d76fbf0b3e778fb5749443a0fb581b127
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-08 13:01:04 -0400

    fix object call

--- cb.py
+++ cb.py
@@ -2,22 +2,17 @@ from cleverbot import Cleverbot
 
 
 class cb:
-    forMember = ''
     cb = {}
 
     def __init__(self):
-        self.forMember = ''
         self.cb['blank'] = Cleverbot()
-
-    def __init__(self, memberid):
-        self.forMember = memberid
-        self.cb[memberid] = Cleverbot()
 
     def getCb(memberid):
         if cb[memberid] is not None:
             return cb[memberid]
         else:
-            return cb['blank']
+            cb[memberid] = Cleverbot()
+            return cb[memberid]
 
     def respond(message, memberid):
         response = cb[memberid].ask(message)
