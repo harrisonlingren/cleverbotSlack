ca33cb15a3bfdd35bc2cca7ce1d134e0d0b51e15
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-08 13:05:13 -0400

    add error handling for json dump

--- listen.py
+++ listen.py
@@ -15,13 +15,25 @@ def hello():
 @app.route('/slackbot', method='POST')
 def slackbot():
     jsonResp = request.json
-    msgText = str(jsonResp['text'])
-    msgSender = str(jsonResp['member'])  # fix this to include correct params for member
+
+    if jsonResp['text'] is not None:
+        msgText = str(jsonResp['text'])
+    else:
+        print("Could not read text")
+        msgText = "Could not read text"
+
+    if jsonResp['text'] is not None:
+        msgSender = str(jsonResp['member'])  # fix this to include correct params for member
+    else:
+        print("Could not get member id")
+        msgText = "Could not get member id"
+
     strDump = json.dumps(jsonResp)
 
     with open('./log.txt', 'wb') as f:
         f.write(bytes(strDump, 'UTF-8'))
     # print(msgText)
+    print(strDump)
 
     cbInstance = cleverbot.getCb(msgSender)
     msgResponse = cbInstance.respond(msgText)
