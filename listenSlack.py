2e34e7b29b286ff0888e0c3cabffb07cb1541f0b
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 14:44:28 -0400

    implement Cleverbot (cb) library

--- listenSlack.py
+++ listenSlack.py
@@ -5,14 +5,21 @@ import slacktools
 
 token = slacktools.getSlackToken()
 sc = SlackSocket(token, translate=True)
+cleverbot = cb()
 
 for event in sc.events():
     data = json.loads(event.json)
     if ((data['type']) == "message"):
         if ((data['channel']) == "harrison-testing"):
+
             msg = (data['text'])
-            # response with msg goes here
-            print("Message found:  " + msg)
+            rsp = cleverbot.getResponse(msgSender, msgText)
+            
+            finalString = "Message: " + msg + "\nResponse: " + rsp + "\n"
+            with open('./log.txt', 'wb') as f:
+                f.write(bytes(finalString, 'UTF-8'))
+
+            print(finalString)
 
         else:
             print("Message is not on the right channel")
