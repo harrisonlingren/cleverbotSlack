fff521b50d2d7b8199979cc3fd2ae95187169e47
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 16:54:06 -0400

    exception handling for KeyErrors (if cleverbot is added to a channel, etc.)

--- listenSlack.py
+++ listenSlack.py
@@ -14,28 +14,35 @@ for event in sc.events():
     data = json.loads(event.json)
     if ((data['type']) == "message"):
 
-        msg = (data['text'])
-        if "<@U1ES5EE1J>" in msg:
-            if ((data['channel']) == "xternsimulator"):
+        try:
+            msg = (data['text'])
+            readSuccess = True
+        except KeyError:
+            print("Error: Could not read message body")
+            readSuccess = False
 
-                msg = msg.replace('<@U1ES5EE1J>', '')
-                msg.replace('@cleverbot:', '')
-                msg = msg.replace('@cleverbot', '')
-                user = (data['user'])
-                rsp = str(cleverbot.getResponse(user, msg))
-                dateNow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if readSuccess:
+            if "<@U1ES5EE1J>" in msg:
+                if ((data['channel']) == "xternsimulator"):
 
-                finalString = "Time: '" + dateNow + "'\nMessage: '" + msg + "'\nUser: '" + user + "'\nBotResponse: '" + rsp + "'\n"
-                with open('./log.txt', 'a') as f:
-                    f.write(finalString)
+                    msg = msg.replace('<@U1ES5EE1J>', '')
+                    msg.replace('@cleverbot:', '')
+                    msg = msg.replace('@cleverbot', '')
+                    user = (data['user'])
+                    rsp = str(cleverbot.getResponse(user, msg))
+                    dateNow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-                ifSent = sc.send_msg(rsp, channel_name="xternsimulator")
+                    finalString = "Time: '" + dateNow + "'\nMessage: '" + msg + "'\nUser: '" + user + "'\nBotResponse: '" + rsp + "'\n"
+                    with open('./log.txt', 'a') as f:
+                        f.write(finalString)
 
-                print(finalString + "\nSent?: " + str(ifSent.sent))
+                    ifSent = sc.send_msg(rsp, channel_name="xternsimulator")
 
+                    print(finalString + "\nSent?: " + str(ifSent.sent))
+
+                else:
+                    print("Message is not on the right channel")
             else:
-                print("Message is not on the right channel")
-        else:
-            print("Cleverbot not called in msg: " + msg)
+                print("Cleverbot not called in msg: " + msg)
 
     time.sleep(1)
