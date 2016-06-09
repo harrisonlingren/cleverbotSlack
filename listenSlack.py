32c09205b56e614d25c861a627f170c0a12e4e69
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 15:24:16 -0400

    post to Slack

--- listenSlack.py
+++ listenSlack.py
@@ -25,7 +25,7 @@ for event in sc.events():
 
             ifSent = sc.send_msg(rsp, channel_name="harrison-testing")
 
-            print(finalString + "\nSent?: " + ifSent.sent)
+            print(finalString + "\nSent?: " + str(ifSent.sent))
 
         else:
             print("Message is not on the right channel")
