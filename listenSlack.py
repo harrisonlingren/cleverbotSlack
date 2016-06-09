7e27164abbf6e510ba0daef5a319f51f5fbcee6a
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 14:38:14 -0400

    add json library

--- listenSlack.py
+++ listenSlack.py
@@ -1,13 +1,13 @@
+import json
 from slacksocket import SlackSocket
 
-import json
 import slacktools
 
 token = slacktools.getSlackToken()
 sc = SlackSocket(token, translate=True)
 
 for event in sc.events():
-    data = json.load(event.json)
+    data = json.load(str(event.json))
     if ((data['type']) == "message"):
         if ((data['channel']) == "harrison-testing"):
             msg = (data['text'])
