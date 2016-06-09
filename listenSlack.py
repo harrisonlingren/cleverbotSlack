ced83141016f79cb15b9ce4c6846b067407ad3f9
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 14:31:29 -0400

    switch to slacksocket library

--- listenSlack.py
+++ listenSlack.py
@@ -6,8 +6,8 @@ token = slacktools.getSlackToken()
 sc = SlackSocket(token, translate=True)
 
 for event in sc.events():
-    if (str(event.json['type'] == "message")):
-        if (str(event.json['channel'] == "harrison-testing")):
+    if (str(event.json['type']) == "message"):
+        if (str(event.json['channel']) == "harrison-testing"):
             msg = str(event.json['text'])
             # response with msg goes here
             print("Message found:  " + msg)
