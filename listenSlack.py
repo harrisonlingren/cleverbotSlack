7d04da027c34099509016aa528a5c6c0d0aff63e
Harrison Lingren <harrisonlingren@gmail.com>
2016-06-09 20:53:51 -0400

    restructure with functions

--- listenSlack.py
+++ listenSlack.py
@@ -6,6 +6,28 @@ from slacksocket import SlackSocket
 import slacktools
 from cb import cb
 
+
+def respondToMessage(msg):
+    if (data['channel']) == "xternsimulator":
+
+                    msg = msg.replace('<@U1ES5EE1J>', '')
+                    msg.replace('@cleverbot:', '')
+                    msg = msg.replace('@cleverbot', '')
+                    user = (data['user'])
+                    rsp = str(cleverbot.getResponse(user, msg))
+                    dateNow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+                    ifSent = sc.send_msg(rsp, channel_name="xternsimulator")
+
+                    finalString = "\nTime: '" + dateNow + "'\nMessage: '" + msg + "'\nUser: '" + user + "'\nBotResponse: '$
+                    with open('./log.txt', 'a') as f:
+                        f.write(finalString)
+
+                    print(finalString)
+
+                else:
+                    print("Message is not on the right channel")
+
 token = slacktools.getSlackToken()
 sc = SlackSocket(token, translate=True)
 cleverbot = cb()
@@ -22,27 +44,12 @@ for event in sc.events():
             readSuccess = False
 
         if readSuccess:
-            if "<@U1ES5EE1J>" in msg:
+            if "<@U1ES5EE1J|cleverbot>" in msg:
+                respondToMessage(msg)
+            elif "<@U1ES5EE1J>" in msg:
+                respondToMessage(msg)
                 if (data['channel']) == "xternsimulator":
 
-                    msg = msg.replace('<@U1ES5EE1J>', '')
-                    msg.replace('@cleverbot:', '')
-                    msg = msg.replace('@cleverbot', '')
-                    user = (data['user'])
-                    rsp = str(cleverbot.getResponse(user, msg))
-                    dateNow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-                    finalString = "\nTime: '" + dateNow + "'\nMessage: '" + msg + "'\nUser: '" + user + "'\nBotResponse: '" + rsp + "'\nSent?: " + str(
-                        ifSent.sent)
-                    with open('./log.txt', 'a') as f:
-                        f.write(finalString)
-
-                    ifSent = sc.send_msg(rsp, channel_name="xternsimulator")
-
-                    print(finalString)
-
-                else:
-                    print("Message is not on the right channel")
             else:
                 print("Cleverbot not called in msg: " + msg)
 
